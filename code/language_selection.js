381e9d8bed16153496ca088cde5dbce2e13f7108
annababiak <babiakanna18@gmail.com>
2023-05-25 16:24:53 +0000

    added user comments

--- code/language_selection.js
+++ code/language_selection.js
@@ -14,10 +14,14 @@ const languageData = {
         Julia: "Юлія",
         Hellena: "Еллена",
         Roman: "Роман",
-        Laras_comment: "",
-        Julia_comment: "",
-        Hellena_comment: "",
-        Roman_comment: "",
+        Lara_comment:
+        "Я просто в захваті від цього скорочувача посилань! Він надзвичайно легкий у використанні, і я завжди можу розраховувати на нього, коли мені потрібно зробити посилання більш коротким і зручним для моїх клієнтів. Я також дуже задоволена його швидкістю! Я рекомендую цей скорочувач посилань всім, хто шукає простий і надійний спосіб скоротити свої URL-адреси. Ви не пошкодуєте!",
+      Julia_comment:
+        "Цей скорочувач посилань - справжнє відкриття для мене. Він простий у використанні, ефективний та безпечний. Я вже рекомендувала його своїм друзям, і вони так само задоволені результатами. Дякую команді сайту за такий чудовий інструмент!",
+      Hellena_comment:
+        "Легко, швидко, зручно! Цей скорочувач посилань - просто чудовий. Все, що потрібно, це вставити посилання і один клік - і ви отримуєте короткий URL. Не знаю, як я раніше обходилася без нього!",
+      Roman_comment:
+        "Користуюся цим скорочувачем посилань протягом кількох місяців і повністю задоволений. Він працює швидко і надійно, ніколи не було проблем з посиланнями. Дуже рекомендую для всіх, хто шукає ефективний спосіб скоротити свої URL-адреси.",
         write_to_support: "служба підтримки",
         Login_page: "Сторінка входу",
         login_header: "Увійдіть до вашого <br/> облікового запису",
@@ -56,10 +60,10 @@ const languageData = {
         Julia: "Julia",
         Hellena: "Hellena",
         Roman: "Roman",
-        Laras_comment: "",
-        Julia_comment: "",
-        Hellena_comment: "",
-        Roman_comment: "",
+        Lara_comment: "I'm simply amazed by this URL shortener! It's incredibly easy to use, and I can always rely on it when I need to make a link shorter and more convenient for my clients. I'm also very pleased with its speed! I recommend this URL shortener to anyone who's looking for a simple and reliable way to shorten their URLs. You won't regret it!",
+        Julia_comment: "This link shortener is a real revelation for me. It is easy to use, effective and safe. I have already recommended it to my friends and they are equally happy with the results. Thanks to the site team for such a great tool!",
+        Hellena_comment: "Easy, fast, convenient! This link shortener is simply awesome. All you need is to paste the link and one click - and you get a short URL. I don't know how I managed without him before!",
+        Roman_comment: "I have been using this link shortener for several months now and am completely satisfied. It works fast and reliable, never had any problems with links. Highly recommended for anyone looking for an effective way to shorten their URLs.",
         write_to_support: "write to support",
         Login_page: "Login page",
         login_header: "Log in to <br /> your account",
