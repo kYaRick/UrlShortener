9de9748a90a91e0c400de6cc8ad7ebe0da45ae52
kYaRick <kilyshuk@gmail.com>
2023-06-10 22:59:28 +0300

    - HostedFileItem was updated to Ukrainian. - auth window was translated into Ukrainian.

--- src/components/HostedFileItem.tsx
+++ src/components/HostedFileItem.tsx
@@ -1,21 +1,23 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { Box, Button, Input, Stack, Text } from "@chakra-ui/react";
 
 import { useViewController } from "../ViewController";
 
 export const HostedFileItem: React.FC<{ wordCode: string; filename: string }> = ({ wordCode, filename }) => {
+  const { i18n } = useTranslation();
   const [, setView] = useViewController();
 
   return (
     <Stack spacing={4} borderWidth={"1px"} borderRadius={"md"} p={4}>
       <Box>
         <Text fontWeight={"medium"} fontSize={"md"}>
-          File
+          {i18n.t("host_file.file")}
         </Text>
         <Input value={filename} isReadOnly variant={"flushed"} />
       </Box>
       <Button colorScheme={"violet"} onClick={() => setView({ slug: "file", params: { wordCode } })}>
-        View file info
+        {i18n.t("host_file.info")}
       </Button>
     </Stack>
   );
