42955b87ff5f6bcf85e94b90536c4e49bcb30b45
kYaRick <kilyshuk@gmail.com>
2023-06-10 22:59:28 +0300

    - HostedFileItem was updated to Ukrainian. - auth window was translated into Ukrainian.

--- src/views/auth.tsx
+++ src/views/auth.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "preact/hooks";
+import { useTranslation } from "react-i18next";
 
 import {
   Button,
@@ -24,6 +25,7 @@ import AuthService from "../services/auth";
 import ErrorHandlingService from "../services/error-handling";
 import toast from "../util/toast";
 
+
 const _SignInBtn = ({
   name,
   color,
@@ -39,20 +41,24 @@ const _SignInBtn = ({
     <VStack>
       {icon}
       <Text fontSize={"xs"} display={{ base: "none", sm: "initial" }}>
-        {name} Sign-in
+        {name} {""}
       </Text>
     </VStack>
   </Button>
 );
 
 export default function AuthView({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
+  const { i18n } = useTranslation();
+  const initialRef = useRef();
+  const { colorMode } = useColorMode();
+  const isAnonymous = AuthService.I.userInfo$.value?.isAnonymous ?? true;
   const [signInState, signInFn] = useAsyncFn(async function signIn(provider: "google" | "github" | "apple") {
     signInState.error = undefined;
 
     try {
       await AuthService.I.signInWithProvider(provider);
     } catch (e) {
-      ErrorHandlingService.I.notifyUserOfError("Error signing in", e);
+      ErrorHandlingService.I.notifyUserOfError(i18n.t("auth.fail"), e);
       return;
     }
   });
@@ -63,22 +69,16 @@ export default function AuthView({ isOpen, onClose }: { isOpen: boolean; onClose
     try {
       await AuthService.I.signOut();
       toast({
-        title: "Successfully signed out!",
+        title: i18n.t("auth.success"),
         status: "success",
         isClosable: true,
       });
       onClose();
     } catch (e) {
-      ErrorHandlingService.I.notifyUserOfError("Error signing out", e);
+      ErrorHandlingService.I.notifyUserOfError(i18n.t("auth.fail"), e);
       return;
     }
   });
-
-  const initialRef = useRef();
-
-  const { colorMode } = useColorMode();
-
-  const isAnonymous = AuthService.I.userInfo$.value?.isAnonymous ?? true;
 
   return (
     <>
@@ -86,13 +86,13 @@ export default function AuthView({ isOpen, onClose }: { isOpen: boolean; onClose
         <ModalOverlay />
         <ModalContent mx={3}>
           <ModalHeader>
-            <Heading fontSize={"xl"}>Manage Account</Heading>
+            <Heading fontSize={"xl"}>{i18n.t("auth.window_title")}</Heading>
           </ModalHeader>
           <ModalCloseButton onClick={onClose} />
           <ModalBody>
             {!isAnonymous ? (
               <Text>
-                Signed in as{" "}
+                {i18n.t("auth.sign_title")}{" "}
                 <Text as={"span"} fontWeight={"bold"}>
                   {`${AuthService.I.userInfo$.value!.email} (${
                     { "google.com": "Google", "github.com": "Github", "apple.com": "Apple" }[
@@ -129,7 +129,7 @@ export default function AuthView({ isOpen, onClose }: { isOpen: boolean; onClose
                   color={`gray.${colorMode === "light" ? 500 : 400}`}
                   my={3}
                 >
-                  Choose a sign-in method
+                  {i18n.t("auth.choose")}
                 </Text>
               </>
             )}
@@ -137,7 +137,7 @@ export default function AuthView({ isOpen, onClose }: { isOpen: boolean; onClose
           {!isAnonymous && (
             <ModalFooter>
               <Button colorScheme={"red"} isLoading={signOutState.loading} onClick={() => signOutFn()}>
-                Sign Out
+                {i18n.t("auth.sign_out")}
               </Button>
             </ModalFooter>
           )}
